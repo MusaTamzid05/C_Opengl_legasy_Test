f0690a16b071a165877f610668274813fb37c5cb
Musa Khan <tamzid101@gmail.com>
2018-04-26 18:19:35 +0600

    We have a prototype of road.

--- src/road.c
+++ src/road.c
@@ -13,8 +13,8 @@ Shape* create_road() {
     ShapeData* shape_data = create_shape_data(RoadCube , 12 , create_vector(1.0f , 1.0f , 1.0f));
 
     Vector3* translation = create_vector(0.0f , 0.0f , 0.0f);
-    Vector3* rotation = create_vector(0.0f , 0.0f , 0.0f);
-    float angle = 0.0f;
+    Vector3* rotation = create_vector(1.0f , 0.0f , 0.0f);
+    float angle = -53.0f;
     MoveData* move_data  = NULL; // could be fixed latter for better motion.
 
 
@@ -27,15 +27,22 @@ Shape* create_road() {
 
 void update_road(Shape* shape ,unsigned char key) {
 
+    if(key == 's')
+        shape->angle += 1;
+
+
+    else if(key == 'w')
+        shape->angle -= 1;
+
 
 }
 
 void draw_road(Shape* shape ) {
 
-    printf("Drawing the road.\n");
 
+    printf("%f\n" , shape->angle);
 
-    draw_rect(-2.7 , 2.7 , 2.7 , -2.7 , create_vector(1.0f , 0.0f , 1.0f));
+    draw_rect(-2.7 , 2.7 , 2.7 , -2.7 , create_vector(0.0f , 0.0f , 1.0f));
     
     
     glEnd();
